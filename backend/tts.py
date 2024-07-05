1f2eddf6ae4dae7780484a70fd220edc08943082
alina775 <aseidel683@gmail.com>
2024-07-05 14:20:07 +1000

    update tts

--- backend/tts.py
+++ backend/tts.py
@@ -1,13 +1,12 @@
 from gtts import gTTS
 
-classification = "transaction"
-
-if (classification == "transaction"):
-    tts = gTTS('Your transaction has been successfully made')
-elif (classification == "interest rates"):
-    tts = gTTS('Your current interest rate on your savings account is 5.5%')
-elif (classification == "scam check"):
-    tts = gTTS("Scams can take various forms, but some prevalent ones include online shopping scams, phishing scams, investment scams and tech support scams.")
-elif (classification == "unknown intent"):
-    tts = gTTS('Sorry, I cannot help you with this today. Is there something else I can do')
-tts.save('sound/audio.mp3')
+def get_output(classification):
+    if (classification == "transaction"):
+        tts = gTTS('Your transaction has been successfully made')
+    elif (classification == "interest rates"):
+        tts = gTTS('Your current interest rate on your savings account is 5.5%')
+    elif (classification == "scam check"):
+        tts = gTTS("Scams can take various forms, but some prevalent ones include online shopping scams, phishing scams, investment scams and tech support scams.")
+    elif (classification == "unknown intent"):
+        tts = gTTS('Sorry, I cannot help you with this today. Is there something else I can do')
+    tts.save('sound/audio.mp3')
